8cd6238d27413a7225a3ad45dc2ef2995ca2f978
Rafaellauersdorf <117686048+Rafaellauersdorf@users.noreply.github.com>
2023-02-03 11:21:26 -0300

    changed name class

---
+++ ExerciciosJava/estseq001.java
@@ -0,0 +1,26 @@
+package ExerciciosJava;
+
+import java.util.Scanner;
+
+public class estseq001 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int valor1 = sc.nextInt();
+        int valor2 = sc.nextInt();
+        int valor3 = sc.nextInt();
+        int valor4 = sc.nextInt();
+        int valor5 = sc.nextInt();
+        int valor6 = sc.nextInt();
+
+        int soma1 = valor1 + valor2;
+        int soma2 = valor3 + valor4;
+        int soma3 = valor5 + valor6;
+
+        System.out.println("SOMA = " + soma1);
+        System.out.println("SOMA = " + soma2);
+        System.out.println("SOMA = " + soma3);
+    
+        sc.close();    
+    }
+}
--- ExerciciosJava/estseq002.java
+++ ExerciciosJava/estseq002.java
@@ -1,26 +1,29 @@
 package ExerciciosJava;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class estseq002 {
     public static void main(String[] args) {
         
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        int valor1 = sc.nextInt();
-        int valor2 = sc.nextInt();
-        int valor3 = sc.nextInt();
-        int valor4 = sc.nextInt();
-        int valor5 = sc.nextInt();
-        int valor6 = sc.nextInt();
 
-        int soma1 = valor1 + valor2;
-        int soma2 = valor3 + valor4;
-        int soma3 = valor5 + valor6;
 
-        System.out.println("SOMA = " + soma1);
-        System.out.println("SOMA = " + soma2);
-        System.out.println("SOMA = " + soma3);
-    
-        sc.close();    
+        double raio1 = sc.nextDouble();
+        double raio2 = sc.nextDouble();
+        double raio3 = sc.nextDouble();
+
+        double area1 = 3.14159 * Math.pow(raio1,2.0);
+        double area2 = 3.14159 * Math.pow(raio2,2.0);
+        double area3 = 3.14159 * Math.pow(raio3,2.0);
+
+        System.out.printf("Área = %.4f%n", area1);
+        System.out.printf("Área = %.4f%n", area2);
+        System.out.printf("Área = %.4f%n", area3);
+
+
+        sc.close();
     }
+    
 }
