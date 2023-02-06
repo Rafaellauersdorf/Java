4b43de6724607b8bd864f207c4abd01e478e7e80
Rafaellauersdorf <117686048+Rafaellauersdorf@users.noreply.github.com>
2023-02-06 19:32:28 -0300

    Create bee1003.java

---
+++ exerciciosDiversos/exerciciosbeecrowd/bee1003.java
@@ -0,0 +1,17 @@
+package exerciciosDiversos.exerciciosbeecrowd;
+
+import java.util.Scanner;
+
+public class bee1003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int prod = A * B;
+            System.out.println("PROD = " + prod);
+    
+        sc.close();
+    }
+}
