0b51f9f56684d6e4a4ce8d2f85fdd7f0891e031d
Rafaellauersdorf <117686048+Rafaellauersdorf@users.noreply.github.com>
2023-02-07 15:06:53 -0300

    ex java

---
+++ exerciciosDiversos/opatribcumulativa.java
@@ -0,0 +1,21 @@
+package exerciciosDiversos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class opatribcumulativa {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int minutos = sc.nextInt();
+
+        double conta = 50.0;
+        if (minutos > 100) {
+            conta += (minutos - 100) * 2.0;
+        }
+        System.out.printf("Valor da conta = R$ %.2f%n", conta);
+
+        sc.close();
+    }
+}
---
+++ exerciciosDiversos/sintopc.java
@@ -0,0 +1,44 @@
+package exerciciosDiversos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class sintopc {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        String dia;
+
+        switch (x) {
+            case 1:
+                dia = "Domingo";
+                break;
+            case 2:
+                dia = "Segunda-Feira";
+                break;
+            case 3:
+                dia = "Terça-Feira";
+                break;
+            case 4:
+                dia = "Quarta-Feira";
+                break;
+            case 5:
+                dia = "Quinta-Feira";
+                break;
+            case 6:
+                dia = "Sexta-Feira";
+                break;
+            case 7:
+                dia = "Sábado";
+                break;
+            default:
+                dia = "Dia inválido";
+                break;
+        }
+        System.out.println("Dia da semana: "+ dia);
+
+    sc.close(); 
+    }
+}
